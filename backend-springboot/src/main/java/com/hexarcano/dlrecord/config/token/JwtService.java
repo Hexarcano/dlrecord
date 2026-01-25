306600e2b689d20beec58429d2538d8fba948751
Manuel Hernández <mahmaruro@outlook.com>
2026-01-25 04:50:26 -0600

    remove unused imports

--- backend-springboot/src/main/java/com/hexarcano/dlrecord/config/token/JwtService.java
+++ backend-springboot/src/main/java/com/hexarcano/dlrecord/config/token/JwtService.java
@@ -12,8 +12,6 @@ import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtService {
