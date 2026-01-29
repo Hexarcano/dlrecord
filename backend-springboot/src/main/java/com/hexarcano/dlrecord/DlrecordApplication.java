4528a1e89abe7cf906f0a34c6ac78d6964d90d78
Manuel Hernández <mahmaruro@outlook.com>
2026-01-29 09:15:02 -0600

    refactor: extract auditing configuration to separate class
    - Move @EnableJpaAuditing from DlrecordApplication to dedicated AuditingConfig
    - Create separate configuration class for JPA auditing to avoid test conflicts

--- backend-springboot/src/main/java/com/hexarcano/dlrecord/DlrecordApplication.java
+++ backend-springboot/src/main/java/com/hexarcano/dlrecord/DlrecordApplication.java
@@ -2,10 +2,8 @@ package com.hexarcano.dlrecord;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class DlrecordApplication {
 
 	public static void main(String[] args) {
